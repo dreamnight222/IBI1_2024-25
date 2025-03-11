0696f25a3732e469a1891280002ee5ecff080070
dreamnight222 <luboyun93@gmail.com>
2025-03-11 09:09:15 +0100

    Added variables.py for Practical 4, Section 4.1

---
+++ Practical4/variables.py
@@ -0,0 +1,18 @@
+# Walk to bus stop: 15 mins
+a = 15
+# Bus journey: 1 hr 15 mins = 75 mins
+b = 75
+# Total time for bus commute
+c = a + b
+
+# Drive: 1 hr 30 mins = 90 mins
+d = 90
+# Walk from car park: 5 mins
+e = 5
+# Total time for car commute
+f = d + e
+
+# Compare c and f: Which is shorter?
+# c = 90 mins, f = 95 mins, so walking + bus is quicker
+print("Bus commute time:", c, "mins")
+print("Car commute time:", f, "mins")
