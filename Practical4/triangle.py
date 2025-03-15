90e49a7d9db97c2919e759c9576792380efc9623
dreamnight222 <luboyun93@gmail.com>
2025-03-15 02:37:55 +0100

    improved after tutorial

--- Practical4/triangle.py
+++ Practical4/triangle.py
@@ -3,11 +3,11 @@
 # 2. Use a loop to calculate the sum from 1 to n for each n
 # 3. Print the first 10 triangular numbers
 
-# Initialize variables
+# 1. Initialize variables
 n = 1
 total = 0
 
-# Loop to calculate the first 10 triangular numbers
+# 2.Loop to calculate the first 10 triangular numbers
 while n <= 10:
     total = total + n
     print(f"Triangle number T_{n} = {total}")
