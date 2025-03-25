9a92d51e8ad2466bbe447680b6df4196c619a67e
dreamnight222 <luboyun93@gmail.com>
2025-03-25 15:15:45 +0100

    Add special_SIR.py

---
+++ Practical6/special_SIR.py
@@ -0,0 +1,47 @@
+# import necessary libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+# define the variables
+beta = 0.3   # infection probability
+gamma = 0.05 # recovery probability
+
+# make array of all susceptible popuation
+population = np.zeros((100, 100))
+
+# choose one infected person randomly
+outbreak = np.random.choice(range(100), 2)
+population[outbreak[0], outbreak[1]] = 1  # 1=infected
+
+fig = plt.figure(figsize=(6,4), dpi=150)
+ims = []
+# stimulate 100 time points
+for t in range(100):
+    # find the infected
+    infected = np.where(population == 1)
+    infected_coords = list(zip(infected[0], infected[1]))
+    
+    # focus on each outbreak points
+    for x, y in infected_coords:
+        # check the neighbours
+        for dx in [-1, 0, 1]:
+            for dy in [-1, 0, 1]:
+                if dx == 0 and dy == 0:  # skip itself
+                   continue
+                nx, ny = x + dx, y + dy
+                # check if the neighbour is susceptible
+                if 0 <= nx < 100 and 0 <= ny < 100 and population[nx, ny] == 0:
+                    # infection occurs with beta probability.
+                    if np.random.random() < beta:  
+                        population[nx, ny] = 1
+    
+        # recovery occurs with gamma probability
+        if np.random.random() < gamma:
+            population[x, y] = 2  # 2=recovered
+    
+    im = plt.imshow(population, cmap='viridis',interpolation='nearest', animated=True)
+    plt.title(f'time point{t+1}')
+    ims.append([im])
+
+ani = animation.ArtistAnimation(fig, ims, interval=200, blit=True, repeat=False)
+plt.show()
