9adb009199c91d516d746c8a88838a760cce3a45
dreamnight222 <luboyun93@gmail.com>
2025-04-02 08:38:25 +0100

    Add a function that I lost

--- Practical7/spliced_tata_genes.py
+++ Practical7/spliced_tata_genes.py
@@ -6,11 +6,15 @@ def has_tata_box(sequence):
     return len(re.findall(r'TATA[AT]A[AT]', sequence))  
 
 # define a function to check if the sequence has splice_combo
-def is_spliced(sequence, splice_combo):
-    return bool(re.search(splice_combo, sequence))
+def is_spliced(sequence,splice_seq):
+    return bool(re.search(splice_seq, sequence))
 
 # get input from user
 splice_combo = input("Enter splice donor/acceptor combination (GTAG, GCAG, or ATAC): ")
+
+head = splice_combo[:2]
+tail = splice_combo[2:]
+splice_seq = rf'{head}.*?{tail}'
 
 output_file = f"{splice_combo}_spliced_genes.fa"
 
@@ -23,7 +27,7 @@ with open(input_file, 'r') as infile, open(output_file, 'w') as outfile:
         line = line.strip()
         if line.startswith('>'):
             if current_seq:
-                if is_spliced(current_seq, splice_combo):# check if it has splice_comnbo
+                if is_spliced(current_seq, splice_seq):# check if it has splice_comnbo
                     tata_count = has_tata_box(current_seq)# calculate the number of tata boxes(see if it has tata boxes)
                     if tata_count > 0:
                         gene_name = line.split()[0][1:].split('_')[0] 
