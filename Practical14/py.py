0d1e39a3598c9bfd5754fd49ae7854c132f29260
dreamnight222 <luboyun93@gmail.com>
2025-05-20 01:04:51 +0100

    In the last commit I added two files, I forgot to mention py.py file in the commit.

--- Practical14/py.py
+++ Practical14/py.py
@@ -3,7 +3,7 @@ import xml.sax
 from datetime import datetime
 
 def dom_parse(xml_file):
-    # use the DOM to parse XML and count the maximum value of each namespace<is_a>
+# use the DOM to parse XML and count the maximum value of each namespace<is_a>
     start = datetime.now()
     DOMTree = xml.dom.minidom.parse(xml_file)
     terms = DOMTree.getElementsByTagName('term')
@@ -28,7 +28,7 @@ def dom_parse(xml_file):
     return max_data, datetime.now() - start
 
 class SAXHandler(xml.sax.ContentHandler):
-    # SAX processor, which counts the maximum value of each namespace<is_a>
+# SAX processor, which counts the maximum value of each namespace<is_a>
     def __init__(self):
         self.max_data = {
             'molecular_function': {'id': '', 'name': '', 'count': 0},
@@ -70,7 +70,7 @@ class SAXHandler(xml.sax.ContentHandler):
             self.current = {}
 
 def sax_parse(xml_file):
-    # use SAX to parse XML and collect statistics
+# use SAX to parse XML and collect statistics
     start = datetime.now()
     parser = xml.sax.make_parser()
     handler = SAXHandler()
@@ -81,19 +81,19 @@ def sax_parse(xml_file):
 
 xml_file = 'go_obo.xml'
 
-    # DOM
+# DOM
 dom_results, dom_time = dom_parse(xml_file)
 print("DOM Results:")
 for ns, data in dom_results.items():
     print(f"{ns}: {data['id']} ({data['name']}) - {data['count']} <is_a>")
 
-    # SAX
+# SAX
 sax_results, sax_time = sax_parse(xml_file)
 print("\nSAX Results:")
 for ns, data in sax_results.items():
     print(f"{ns}: {data['id']} ({data['name']}) - {data['count']} <is_a>")
 
-    # time comparison
+# time comparison
 print(f"\nDOM: {dom_time}\nSAX: {sax_time}")
 print("# SAX is generally faster because it doesn't require the full file to be loaded into memory for streaming parsing")
 
